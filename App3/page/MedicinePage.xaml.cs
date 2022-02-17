1d1e1b991c915d736c74fef2867f60dec56c290d
遥香 鈴木 <190455@jc-21.jp>
2022-02-17 14:49:26 +0900

    ?

--- App3/page/MedicinePage.xaml.cs
+++ App3/page/MedicinePage.xaml.cs
@@ -3,18 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace NavPageSample.page
+namespace NavPageSample
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MedicinePage : ContentPage
+    public partial class MedicineEntryPage : ContentPage
     {
-        public MedicinePage()
+        public MedicineEntryPage()
         {
             InitializeComponent();
         }
+
+        private async void OnAddButtonClicked(object sender, EventArgs e)
+        {
+
+
+            if (!String.IsNullOrWhiteSpace(Medicine_Name_Entry.Text))
+            {
+                await App.Database.SaveMedicineAsync(new Medicine
+                {
+                    Medicine_name = Medicine_Name_Entry.Text,
+                    Url = Url_Entry.Text,
+                    timing = Timing_Entry.Text,
+                });
+
+                await App.Database.SaveUserAsync(new User
+                {
+                    jikantai = Jikantai_Entry.Text
+                });
+            }
+
+        }
+
+
     }
 }
